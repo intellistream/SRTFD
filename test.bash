ad923293cb05d6b1897d66b7aabc401d9393d8fc
Dandan197 <157668002+Dandan197@users.noreply.github.com>
2024-08-08 20:37:48 +0800

    Update test.bash

--- test.bash
+++ test.bash
@@ -1,19 +1,19 @@
 python3 general_main.py --data HRS --num_tasks 6 --cl_type nc --agent SRTFD --num_runs 1 --N 1000
-python3 general_main.py --data HRS --num_tasks 6 --cl_type vc --agent SRTFD --num_runs 1 --ns_factor 0.4 --N 1000 --n_r 1 --f_r 1
+python3 general_main.py --data HRS --num_tasks 6 --cl_type vc --agent SRTFD --num_runs 1  --N 1000 
 
 python3 general_main.py --data TEP --num_tasks 22 --cl_type nc --agent SRTFD --num_runs 1 --N 1000
-python3 general_main.py --data TEP --num_tasks 22 --cl_type vc --agent SRTFD --num_runs 1 --ns_factor 0.4  --n_r 1 --f_r 1
+python3 general_main.py --data TEP --num_tasks 22 --cl_type vc --agent SRTFD --num_runs 1 --N 1000
 
 python3 general_main.py --data CARLS_S --num_tasks 10 --cl_type nc --agent SRTFD --num_runs 3 --N 1000
-python3 general_main.py --data CARLS_S --num_tasks 10 --cl_type vc --agent SRTFD --num_runs 1 --N 1000  --n_r 1 --f_r 1
+python3 general_main.py --data CARLS_S --num_tasks 10 --cl_type vc --agent SRTFD --num_runs 1 --N 1000  
 
 python3 general_main.py --data CARLS_M --num_tasks 5 --cl_type nc --agent SRTFD --num_runs 3 --N 1000
-python3 general_main.py --data CARLS_M --num_tasks 5 --cl_type vc --agent SRTFD --num_runs 1 --N 1000  --n_r 1 --f_r 1
+python3 general_main.py --data CARLS_M --num_tasks 5 --cl_type vc --agent SRTFD --num_runs 1 --N 1000  
 
 
 # ER
 python3 general_main.py --data HRS --num_tasks 6 --cl_type nc --agent ER --num_runs 1 --N 1000
-python3 general_main.py --data HRS --num_tasks 6 --cl_type vc --agent ER --num_runs 1 --ns_factor 0.4  --n_r 1 --f_r 1
+python3 general_main.py --data HRS --num_tasks 6 --cl_type vc --agent ER --num_runs 1 --N 1000
 
 # ASER
 python3 general_main.py --data HRS --num_tasks 6 --cl_type nc --agent ER --num_runs 1 --N 1000 --update ASER --retrieve ASER --mem_size 5000 --aser_type asvm --n_smp_cls 1.5 --k 3 
